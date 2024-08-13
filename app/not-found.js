d023c6e7436875a442edbb3dc4566d0787f19571
adixadymak <adixadymak@gmail.com>
2024-08-13 14:15:15 +0100

    Loading & Rendering Meal Details via Dynamic Routes & Route Parameters

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>No Found</h1>
+      <p>Unfortunately, we could not find the requested page or resource.</p>
+    </main>
+  );
+}
