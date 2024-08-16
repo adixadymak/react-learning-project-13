4d80d5244a249a7fdc9ae7814ebdff7641936f3b
adixadymak <adixadymak@gmail.com>
2024-08-16 02:04:45 +0100

    Adding an Image Preview to the Picker

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,19 +1,46 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import classes from "./image-picker.module.css";
+import Image from "next/image";
 
 export default function ImagePicker({ label, name }) {
   const imageInputRef = useRef();
+  const [pickedImage, setPickdImage] = useState();
 
   function handlePickClick() {
     imageInputRef.current.click();
+  }
+
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+    if (!file) {
+      return;
+    }
+
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      setPickdImage(fileReader.result);
+    };
+
+    fileReader.readAsDataURL(file);
   }
 
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No image picked yet.</p>}
+          {pickedImage && (
+            <Image
+              src={pickedImage}
+              alt="The image selected by the user"
+              fill
+            />
+          )}
+        </div>
         <input
           className={classes.input}
           type="file"
@@ -21,6 +48,8 @@ export default function ImagePicker({ label, name }) {
           accept="image/png, image/jpeg"
           name={name}
           ref={imageInputRef}
+          //add 'multiple' if you want user to add multiple files
+          onChange={handleImageChange}
         />
         <button
           className={classes.button}
